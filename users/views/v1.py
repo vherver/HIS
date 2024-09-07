308da1172c6c178c1efa9b188b15c6d59a09c66f
Victor Herver <victor@mercanto.mx>
2024-09-06 22:06:49 -0600

    Add notification

--- users/views/v1.py
+++ users/views/v1.py
@@ -1,6 +1,7 @@
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.authentication import get_authorization_header
 
 from users.models import BlacklistedToken
 from users.serializers.v1 import ChangePasswordSerializer, LogoutSerializer
@@ -32,14 +33,11 @@ class LogoutView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request):
-        serializer = LogoutSerializer(data=request.data)
-        if serializer.is_valid():
-            token = serializer.validated_data["token"]
+        auth_header = get_authorization_header(request).decode('utf-8')
+        token = auth_header.split('Bearer ')[1]
 
-            BlacklistedToken.objects.create(token=token)
+        BlacklistedToken.objects.create(token=token)
 
-            return Response(
-                {"detail": "Successfully logged out"}, status=status.HTTP_204_NO_CONTENT
-            )
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
